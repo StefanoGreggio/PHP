6b918e448eab6133f9cfbdaadcee30324ced4448
StefanoGreggio <stefano.greggio.1f@iisviolamarchesini.edu.it>
2023-09-22 18:43:14 +0200

    soluzione esercizi array

--- array/es_2.php
+++ array/es_2.php
@@ -4,14 +4,12 @@ $array=[];
 $nElementi=5;
 
 for($i=0;$i<$nElementi;$i++){
-    echo "inserire il"+ $i +"° numero all'interno dell'array: ";
+    echo "inserire il ". $i+1 ."° numero all'interno dell'array: ";
     $array[$i]=readline();
     echo "\n";
 }
 
 echo "\nl'array contiene i seguenti numeri: ";
 for($i=count($array);$i>0;$i--){
-    echo  $array[$i]+"\n";
+    echo  $array[$i]."\n";
 }
-
-
--- array/es_5.php
+++ array/es_5.php
@@ -1,20 +1,20 @@
 <?php
 
 $array=[];
-$nElementi=10;
+$nElementi=3;
 
 for($i=0;$i<$nElementi;$i++){
-    echo "inserire il"+ $i +"° numero all'interno dell'array: ";
+    echo "inserire il ". $i+1 ."° numero all'interno dell'array: ";
     $array[$i]=readline();
     echo "\n";
 }
 
 $ricerca;
 
-echo  "inserire un numero da ricercare all'interno dell'array";
+echo  "inserire un numero da ricercare all'interno dell'array ";
 $ricerca=readline();
 for($i=0;$i<count($array);$i++){
     if($array[$i]==$ricerca){
-        echo "il numero "+ $ricerca + "è stato trovato in posizione "+ $i;
+        echo "il numero " . $ricerca . "è stato trovato in posizione ". $i;
     }
 }
