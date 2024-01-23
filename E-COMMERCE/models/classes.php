cd824519a8a637d8a4f15322d8930b183b0ea8a5
StefanoGreggio <98811668+StefanoGreggio@users.noreply.github.com>
2024-01-23 23:35:56 +0100

    Delete E-COMMERCE directory

--- E-COMMERCE/models/classes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__ . '/Session.php';
-require_once __DIR__ . '/User.php';
-require_once __DIR__ . '/../connection/DbManager.php';
-require_once __DIR__ . '/Product.php';
-require_once __DIR__ . '/Cart.php';
-
-?>
