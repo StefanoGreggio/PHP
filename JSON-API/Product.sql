c8909d3542afe4548262437d1e098b23a3581efa
StefanoGreggio <98811668+StefanoGreggio@users.noreply.github.com>
2024-02-29 09:19:46 +0100

    Delete JSON-API directory

--- JSON-API/Product.sql
+++
@@ -1,8 +0,0 @@
-create database if not exists ecommerce;
-create table if not exists ecommerce.products
-(
-    id     int not null auto_increment primary key,
-    nome   varchar(50),
-    prezzo float,
-    marca  varchar(50)
-);
